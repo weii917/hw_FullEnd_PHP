d747a6c08b6f3ef10ba34019460b41e7b9b2a866
wei <wendyhsu09@gmail.com>
2024-01-18 21:05:08 +0800

    修改

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>卓越科技大學校園資訊系統</title>
+	<title>貓旅館</title>
 	<!-- font -->
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
